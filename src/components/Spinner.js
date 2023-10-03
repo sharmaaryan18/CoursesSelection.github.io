6188ec505ab68d3642303188298d504d9fc79f31
sharmaaryan18 <aryansharm3876@gmail.com>
2023-10-04 02:32:52 +0530

    CoursesSelectionSite

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+
+import "./Spinner.css"
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className="flex flex-col items-center space-y-2">
+        <div className='spinner'></div>
+        <p className="text-bgDark text-lg font-semibold">Loading.....</p>
+    </div>
+  )
+}
+
+export default Spinner
